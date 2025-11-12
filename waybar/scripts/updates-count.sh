13341f08f23d11b0a3f3675d6ba4b2f34b17e158
Leandro de O Barbosa <leandrodeo.barbosa@gmail.com>
2025-11-12 02:09:05 -0300

    fix(waybar): correções e adaptações em chamadas de scripts

--- waybar/scripts/updates-count.sh
+++ waybar/scripts/updates-count.sh
@@ -5,6 +5,6 @@ count=$(dnf check-update --refresh 2>/dev/null | grep -E '^[a-zA-Z0-9]' | wc -l)
 if [ "$count" -eq 0 ]; then
     echo " Fedora is up-to-date"
 else
-    echo " $count atualizações disponíveis"
+    echo " $count updates available"
 fi
 
